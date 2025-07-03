524ee2a36c62891a5ea76b9229dec51d0a1d96ba
Karturbis <118579678+Karturbis@users.noreply.github.com>
2025-07-03 23:52:38 +0200

    change window size, put the code into a class, add support for two saves of the stop watch

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 import time
 
 #consts:
-WINDOW_SIZE = (1920, 860)
+WINDOW_SIZE = (1920, 1000)
 BG_COLOR = "black"
 FG_COLOR = "green"
 FG_COLOR_2 = "white"
@@ -12,6 +12,7 @@ FG_COLOR_WARNING = "yellow"
 FG_COLOR_CRITICAL = "red"
 BG_COLOR_TIME_OVER = "red"
 FG_COLOR_TIME_OVER = "black"
+DELTATIME_POSITION: tuple = (WINDOW_SIZE[0]/16, WINDOW_SIZE[1]/2)
 # 3 min = 180s
 # 4 min = 240s
 # 5 min = 300s
@@ -28,89 +29,128 @@ TIME_JUMP_BIG = 30
 SCREEN = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Presentation Timer")
 
-#init
-pygame.init()
-pygame.font.init()
-pygame.display.init()
-clock = pygame.time.Clock()
-icon = pygame.image.load("icon.png")
-pygame.display.set_icon(icon)
 
-# Initialize fonts:
-font = pygame.font.Font("freesansbold.ttf", 200)
-font_big = pygame.font.Font("freesansbold.ttf", 420)
+class Clock():
 
-def draw_time():
-    time_now = time.localtime()
-    time_now = time.strftime("%H:%M:%S", time_now)
-    text_score_headline = font.render(str(time_now), True, FG_COLOR_2)
-    SCREEN.blit(
-        text_score_headline,
-        (WINDOW_SIZE[0]/3, WINDOW_SIZE[1]/4)
-        )
+    def __init__(self):
+        # initialize pygame:
+        pygame.init()
+        pygame.font.init()
+        pygame.display.init()
+        self.clock = pygame.time.Clock()
+        icon = pygame.image.load("icon.png")
+        pygame.display.set_icon(icon)
+        # Initialize fonts:
+        self.font = pygame.font.SysFont("mono", 200)
+        self.font_big = pygame.font.SysFont("mono", 350)
+        self.delta_time = 0
+        # init placement variables:
+        # variables:
+        self.saved_times: list = [0, 0]
+        self.localtime_position: tuple = (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/4)
+        self.savetime_positions: list[tuple] = [
+            (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/4),
+            (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/4),
+            ]
 
-def draw_delta_time(delta_time, running):
-    print_color = FG_COLOR_2
-    if running:
-        print_color = FG_COLOR
-        if delta_time >= WARNING_TIME_SEC:
-            print_color = FG_COLOR_WARNING
-        if delta_time >= CRITICAL_TIME_SEC:
-            print_color = FG_COLOR_CRITICAL
-        if delta_time >= TIME_OVER_SEC:
-            print_color = FG_COLOR_TIME_OVER
-    delta_time = time.strftime("%H:%M:%S" , time.gmtime(delta_time))
-    text_score_headline = font_big.render(str(delta_time), True, print_color)
-    SCREEN.blit(
-        text_score_headline,
-        (WINDOW_SIZE[0]/16, WINDOW_SIZE[1]/2)
-        )
 
-def main():
-    start_time = 0
-    end_time = 0
-    running: bool = False
-    while True:
-        for event in pygame.event.get(): # event listener
-            # making the window closable:
-            if event.type == pygame.QUIT:
-                pygame.QUIT
-                exit(0)
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
+    def draw_time(self):
+        time_now = time.strftime("%H:%M:%S", time.localtime())
+        text = self.font.render(str(time_now), True, FG_COLOR_2)
+        SCREEN.blit(text, self.localtime_position)
+
+    def draw_saved_times(self):
+        for e, saved_time in enumerate(self.saved_times):
+            if saved_time:
+                text = self.font.render(saved_time, True, FG_COLOR_2)
+                SCREEN.blit(text, self.savetime_positions[e])
+
+    def draw_delta_time(self, delta_time, running):
+        print_color = FG_COLOR_2
+        if running:
+            print_color = FG_COLOR
+            if delta_time >= WARNING_TIME_SEC:
+                print_color = FG_COLOR_WARNING
+            if delta_time >= CRITICAL_TIME_SEC:
+                print_color = FG_COLOR_CRITICAL
+            if delta_time >= TIME_OVER_SEC:
+                print_color = FG_COLOR_TIME_OVER
+        delta_time = time.strftime("%H:%M:%S" , time.gmtime(delta_time))
+        text = self.font_big.render(str(delta_time), True, print_color)
+        SCREEN.blit(text, DELTATIME_POSITION)
+
+    def main(self):
+        start_time = 0
+        end_time = 0
+        running: bool = False
+        while True:
+            for event in pygame.event.get(): # event listener
+                # making the window closable:
+                if event.type == pygame.QUIT:
                     pygame.QUIT
                     exit(0)
-                elif event.key == pygame.K_SPACE:
-                    if not running:
-                        start_time = int(time.time())-end_time+start_time # round, so both clocks update at the same time
-                        running = True
-                    else:
-                        running = False
-                        end_time = int(time.time())
-                elif event.key == pygame.K_r:
-                    start_time = int(time.time())
-                    end_time = int(time.time())
-                elif event.key == pygame.K_LEFT or event.key == pygame.K_DOWN:
-                    start_time += TIME_JUMP_BIG
-                    if int(time.time())-start_time < 0:
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        pygame.QUIT
+                        exit(0)
+                    elif event.key == pygame.K_SPACE:
+                        if not running:
+                            start_time = int(time.time())-end_time+start_time # round, so both clocks update at the same time
+                            running = True
+                        else:
+                            running = False
+                            end_time = int(time.time())
+                    elif event.key == pygame.K_r:
                         start_time = int(time.time())
-                    if int(end_time-start_time) < 0 and not running:
-                        start_time = end_time
-                elif event.key == pygame.K_RIGHT or event.key == pygame.K_UP:
-                    start_time -= TIME_JUMP_BIG
-        delta_time = time.time()-start_time
-        if running and delta_time >= TIME_OVER_SEC:
-            bg_color = BG_COLOR_TIME_OVER
-        else:
-            bg_color = BG_COLOR
-        SCREEN.fill(bg_color)
-        if running:
-            draw_delta_time(delta_time, running)
-        else:
-            draw_delta_time(end_time-start_time, running)
-        draw_time()
-        pygame.display.flip()
-        clock.tick(FPS)
+                        end_time = int(time.time())
+                    elif event.key == pygame.K_LEFT or event.key == pygame.K_DOWN:
+                        start_time += TIME_JUMP_BIG
+                        if int(time.time())-start_time < 0:
+                            start_time = int(time.time())
+                        if int(end_time-start_time) < 0 and not running:
+                            start_time = end_time
+                    elif event.key == pygame.K_RIGHT or event.key == pygame.K_UP:
+                        start_time -= TIME_JUMP_BIG
+                    elif event.key == pygame.K_1:
+                        # save the current stopwatch time to slot 1
+                        if running:
+                            self.saved_times[0] = str(time.strftime("%H:%M:%S" , time.gmtime(self.delta_time)))
+                        else:
+                            self.saved_times[0] = str(time.strftime("%H:%M:%S" , time.gmtime(end_time-start_time)))
+                        # update clock positions
+                        if self.saved_times[1]:
+                            self.savetime_positions[0] = (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/6)
+                            self.savetime_positions[1] = (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/3)
+                            self.localtime_position = (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/128)
+                        else:
+                            self.savetime_positions[0] = (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/3)
+                            self.localtime_position = (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/8)
+                    elif event.key == pygame.K_2:
+                         # save the current stopwatch time to slot 2
+                        if running:
+                            self.saved_times[1] = str(time.strftime("%H:%M:%S" , time.gmtime(self.delta_time)))
+                        else:
+                            self.saved_times[1] = str(time.strftime("%H:%M:%S" , time.gmtime(end_time-start_time)))
+                        # update clock positions
+                        self.savetime_positions[0] = (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/6)
+                        self.savetime_positions[1] = (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/3)
+                        self.localtime_position = (WINDOW_SIZE[0]/4, WINDOW_SIZE[1]/128)
+
+            self.delta_time = time.time()-start_time
+            if running and self.delta_time >= TIME_OVER_SEC:
+                bg_color = BG_COLOR_TIME_OVER
+            else:
+                bg_color = BG_COLOR
+            SCREEN.fill(bg_color)
+            if running:
+                self.draw_delta_time(self.delta_time, running)
+            else:
+                self.draw_delta_time(end_time-start_time, running)
+            self.draw_time()
+            self.draw_saved_times()
+            pygame.display.flip()
+            self.clock.tick(FPS)
 
 if __name__ == "__main__":
-    main()
+    clock = Clock()
+    clock.main()
